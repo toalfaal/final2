1375f18ee660f6f01cfd4ff908a991c49f2e9b87
alinsrs <alinaseri797979@gmail.com>
2020-07-19 01:19:28 +0430

    DataBase

--- src/DataBase/DataBaseHelper.java
+++ src/DataBase/DataBaseHelper.java
@@ -11,7 +11,7 @@ public class DataBaseHelper{
 
     public static void getConnection(){
         Class.forName.getConnection("jdbc:mysql://localhost/3306/");
-        con = DriverManager
+        con = DriverM
 
     }
 
