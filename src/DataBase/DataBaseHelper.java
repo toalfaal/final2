b438d4c9ba8486b512590488a6cf6cf2fcfbfdee
alinsrs <alinaseri797979@gmail.com>
2020-07-19 15:45:14 +0430

    DataBase

--- src/DataBase/DataBaseHelper.java
+++ src/DataBase/DataBaseHelper.java
@@ -1,50 +1,156 @@
+package DataBase;
+
 import java.sql.*;
 
 public class DataBaseHelper {
-    public DataBaseHelper {
-        private Connection con;
-        private statement stt;
-        private String url = "jdbc:mysql://localhost/3306/";
-        private String username = "root";
-        private String password = "";
+
+    private Connection con;
+    private Statement stt;
+    private String url = "jdbc:mysql://localhost/3306/";
+    private String username = "root";
+    private String password = "";
+
+    public DataBaseHelper() {
+        try {
+            getConnection();
+            createTableForEmployee();
+            writeToTableForEmployee();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static void getConnection() {
-        Class.forName.getConnection("com.mysql.jdbc.Driver").newInstance();
+    public void getConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
         con = DriverManager.getConnection(url, username, password);
 
-        stt = con.ceateStatement();
+        stt = con.createStatement();
         stt.execute("CREATE DATABASE IF NOT EXISTS 2alfagp");
         stt.execute("USE 2alfagp");
     }
 
-    public static void createTableForEmployee() {
+    public void createTableForEmployee() throws SQLException {
 
         stt.execute("DROP TABLE IF EXISTS Employee");
-        stt.execute("CREATE TABLE people (id BIGINT NOT NULL AUTO_INCREMENT , fName, lName, eFName, eLName," +
+        stt.execute("CREATE TABLE Employee (id BIGINT NOT NULL AUTO_INCREMENT , fName, lName, eFName, eLName," +
                 " idNumber, cellphoneNumber, homeNumber, employmentYear, employmentMonth, employmentDay, employmentIDNumber, childCount," +
                 "maritalStatus BOOLEAN, categoryNameP, categoryNameE, postNameP, postNameE, employeeSerialCode, jobCategorySerialCode)");
     }
 
-    public static void writeToTableForEmployee() {
+    public void writeToTableForEmployee() throws SQLException {
         stt.execute("INSERT INTO Employee (id BIGINT NOT NULL AUTO_INCREMENT , fName, lName, eFName, eLName," +
                 "idNumber, cellphoneNumber, homeNumber, employmentYear, employmentMonth, employmentDay, employmentIDNumber, childCount" +
-                " maritalStatus BOOLEAN, categoryNameP, categoryNameE, postNameP, postNameE, employeeSerialCode, jobCategorySerialCode) VALUES ...")
-                //nages
+                " maritalStatus BOOLEAN, categoryNameP, categoryNameE, postNameP, postNameE, employeeSerialCode, jobCategorySerialCode) VALUES ...");
+        //nages
     }
 
-    public static void readFromTableForEmployee(){
-        ResultSet res = stt.executeQuery("SELECT * FROM people WHERE fName = '...'");
+    public void readFromTableForEmployee() throws SQLException {
+        ResultSet res = stt.executeQuery("SELECT * FROM Employee WHERE employeeSerialCode = '...'");
 
-        while(res.next()){
-            System.out.println(res.getString("fName") + "  " + res.getInt("id"));
-            //...
-            //nages
+        while (res.next()) {
+            String namePersian = res.getString("fName");
+            String lastNamePersian = res.getString("lName");
+            String nameEnglish = res.getString("eFName");
+            String lastNameEnglish = res.getString("eLName");
+            String idNumber = res.getString("idNumber");
+            String cellphoneNumber = res.getString("cellphoneNumber");
+            String homeNumber = res.getString("homeNumber");
+            int employmentYear = res.getInt("employmentYear");
+            int employmentMonth = res.getInt("employmentMonth");
+            int employmentDay = res.getInt("employmentDay");
+            long employmentIDNumbe = res.getLong("employmentIDNumbe");
+            int childCount = res.getInt("childCount");
+            boolean maritalStatus = res.getBoolean("maritalStatus");
+            String categoryNamePersian = res.getString("categoryNameP");
+            String categoryNameEnglish = res.getString("categoryNameE");
+            String postNamePersian = res.getString("postNameP");
+            String postNameEnglish = res.getString("postNameE");
+            String employeeSerialCode = res.getString("employeeSerialCode");
+            String jobCategorySerialCode = res.getString("jobCategorySerialCode");
+
         }
     }
 
+    public void createTableJobCategory() throws SQLException {
+        stt.execute("DROP TABLE IF EXISTS jobCategory");
+        stt.execute("CREATE TABLE jobCategory (categoryNamePersian, categoryNameEnglish, postNamePersian, postNameEnglish, jobCategorySerialCode");
+    }
+
+    public void writeToTableJobCategory() throws SQLException {
+        stt.execute("INSERT INTO jobCategory (categoryNamePersian, categoryNameEnglish, postNamePersian, postNameEnglish, jobCategorySerialCode)" +
+                "VALUES ... ");
+        //NAGES
+    }
+
+    public void readFromTableJobCategory() throws SQLException {
+
+        ResultSet res = stt.executeQuery("SELECT * FROM TableJobCategory WHERE jobCategorySerialCode = ...");
+
+        while (res.next()) {
+
+            String categoryNamePersian = res.getString("categoryNamePersian");
+            String categoryNameEnglish = ("categoryNameEnglish");
+            String postNamePersian = ("postNamePersian");
+            String postNameEnglish = ("postNameEnglish");
+            String JobCategorySerialCode = ("jobCategorySerialCode");
+        }
+    }
+
+    public void CreateTableLegalReceipt() throws SQLException {
+        stt.execute("DROP TABLE IF EXIST LegalReceipt");
+        stt.execute("CREATE TABLE LegalReceipt (BaseAmount, childAmount, maritalStatusAmount, workExperienceAmount, eextraWorkTimeAmount, totalAdditions, " +
+                "totalDeductions, finalSalary,taxAmount, insuranceAmount, legalReceiptSerialCode)");
+    }
+
+    public void writeToTableLegalReceipt() throws SQLException {
+        stt.execute("INSERT INTO LegalReceipt (BaseAmount, childAmount, maritalStatusAmount, workExperienceAmount, extraWorkTimeAmount," +
+                " totalAdditions,totalDeductions, finalSalary,taxAmount, insuranceAmount, legalReceiptSerialCode) VALUES...");
+    }
+
+    public void readTableLegalReceipt() throws SQLException {
+        ResultSet res = stt.executeQuery("SELECT * FROM LegalReceipt WHERE legalReceiptSerialCode = ...");
+
+        while (res.next()) {
+
+            double BaseAmount = res.getDouble("BaseAmount");
+            double childAmount = res.getDouble("childAmount");
+            double maritalStatusAmount = res.getDouble("maritalStatusAmount");
+            double workExperienceAmount = res.getDouble("workExperienceAmount");
+            double extraWorkTimeAmount = res.getDouble("extraWorkTimeAmount");
+            double totalAdditions = res.getDouble("totalAdditions");
+            double totalDeductions = res.getDouble("totalDeductions");
+            double finalSalary = res.getDouble("finalSalary");
+            double taxAmount = res.getDouble("taxAmount");
+            double insuranceAmount = res.getDouble("insuranceAmount");
+            String legalReceiptSerialCode = res.getString("legalReceiptSerialCode");
+        }
+    }
+
+    public void createTablePayRate() throws SQLException {
+        stt.execute("DROP TABLE IF EXIST PayRate");
+        stt.execute("CREATE TABLE PayRate(BaseRate, childRate, maritalStatusRate, workExperienceRate," +
+                " extraWorkTimeRate, taxRate, insuranceRate, payRateSerialCode)");
+    }
+
+    public void writeToTablePayRate() throws SQLException {
+        stt.execute("INSERT INTO PayRate (BaseRate, childRate, maritalStatusRate, workExperienceRate," +
+                "extraWorkTimeRate, taxRate, insuranceRate, payRateSerialCode) VALUES ...");
+    }
+
+    public void readFromTablePayRate() throws SQLException {
+        ResultSet res = stt.executeQuery("SELECT * FROM PayRate WHERE payRateSerialCode = '...'");
+
+        double BaseRate = res.getDouble("BaseRate");
+        double childRate = res.getDouble("childRate");
+        double maritalStatusRate = res.getDouble("maritalStatusRate");
+        double WorkExperienceRate = res.getDouble("workExperienceRate");
+        double extraWorkTimeRate = res.getDouble("extraWorkTimeRate");
+        double taxRate = res.getDouble("taxRate");
+        double insuranceRate = res.getDouble("insuranceRate");
+        String payRateSerialCode = res.getString("payRateSerialCode");
+    }
 
     public static void main(String[] args) {
-
+        new DataBaseHelper();
     }
 }
